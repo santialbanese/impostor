017a21224d28c0d51ab27001863a51fbee4ddadd
Santiago Albanese <santialbanese11@gmail.com>
2025-08-24 13:21:18 -0300

    adding changes

--- public/assets/js/topics.js
+++ public/assets/js/topics.js
@@ -1,64 +1,85 @@
+// public/assets/js/topics.js
 export const topics = {
   // ===== Categorías raíz =====
-  animales: ["perro","gato","elefante","león","tigre","oso","conejo","pájaro","pez","caballo","serpiente","pato","cocodrilo","jirafa","cebra","hipopótamo","rinoceronte","mono","lobo","zorro","ciervo","ardilla","ratón","murciélago","delfín","ballena","foca","koala","canguro","panda","águila","búho","pingüino","flamenco","loro","cisne","gallina","gallo","pavo","tortuga","iguana","lagarto","camaleón","rana","sapo","salamandra","tiburón","pulpo","medusa","estrella de mar","cangrejo","abeja","mariposa","hormiga","araña","libélula","escarabajo"],
-  películas: ['avatar','titanic','avengers','starwars','batman','superman','spiderman','frozen','shrek','harry potter','el señor de los anillos','jurassic park','jurassic world','el rey león','la bella y la bestia','los increíbles','buscando a nemo','coco','mi villano favorito','minions','madagascar','kung fu panda','cars','intensamente','moana','encanto','doctor strange','iron man','thor','black panther','toy story','Interstellar','Joker','Deadpool','Barbie','Forrest gump','Matrix','El lobo de wall street','Up','Ratatouille','Wall-e','Monsters inc','Aladdin','Piratas del caribe','Rápido y Furioso','Transformers','Maze runner','the silence of the lambs','the social network','a star is born','bohemian rhapsody','once upon a time in hollywood',"Guardianes de la Galaxia",'captain america','ant-man','black widow','john wick','mission: impossible','godzilla','breaking bad','game of thrones','stranger things','dark','the office','friends','black mirror','peaky blinders','narcos','la casa de papel','élite','vis a vis','chernobyl','lost','prison break','vikings',"grey's anatomy",'dragon ball z','el marginal','okupas','merlí'],
+  animales: [
+    "perro","gato","elefante","león","tigre","oso","conejo","pájaro","pez","caballo","serpiente","pato",
+    "cocodrilo","jirafa","cebra","hipopótamo","rinoceronte","mono","lobo","zorro","ciervo","ardilla","ratón",
+    "murciélago","delfín","ballena","foca","koala","canguro","panda","águila","búho","pingüino","flamenco",
+    "loro","cisne","gallina","gallo","pavo","tortuga","iguana","lagarto","camaleón","rana","sapo","salamandra",
+    "tiburón","pulpo","medusa","estrella de mar","cangrejo","abeja","mariposa","hormiga","araña","libélula","escarabajo"
+  ],
+
+  películas: [
+    "avatar","titanic","avengers","starwars","batman","superman","spiderman","frozen","shrek","harry potter",
+    "el señor de los anillos","jurassic park","jurassic world","el rey león","la bella y la bestia","los increíbles",
+    "buscando a nemo","coco","mi villano favorito","minions","madagascar","kung fu panda","cars","intensamente","moana",
+    "encanto","doctor strange","iron man","thor","black panther","toy story","Interstellar","Joker","Deadpool","Barbie",
+    "Forrest gump","Matrix","El lobo de wall street","Up","Ratatouille","Wall-e","Monsters inc","Aladdin",
+    "Piratas del caribe","Rápido y Furioso","Transformers","Maze runner","the silence of the lambs","the social network",
+    "a star is born","bohemian rhapsody","once upon a time in hollywood","Guardianes de la Galaxia","captain america",
+    "ant-man","black widow","john wick","mission: impossible","godzilla","breaking bad","game of thrones","stranger things",
+    "dark","the office","friends","black mirror","peaky blinders","narcos","la casa de papel","élite","vis a vis",
+    "chernobyl","lost","prison break","vikings","grey's anatomy","dragon ball z","el marginal","okupas","merlí"
+  ],
+
   cantantes: [
-    "Bad Bunny","Daddy Yankee","Don Omar","Wisin","Yandel","Rauw Alejandro","Ozuna","Anuel AA",
-    "Tego Calderón","Residente","Luis Fonsi","Ricky Martin","Chayanne","Pedro Capó","Nicky Jam","Farruko","De La Ghetto","Ñengo Flow","Arcángel","Zion","Lennox","Plan b",
-    "Lunay","Myke Towers","Mora","Lyanno","Bryant Myers","Alex Rose","Dalex","Jay Wheeler","Almighty","Cosculluela",
-    "Jowell y Randy","Kendo Kaponi","Anonimus","Young Miko","Villano Antillano","YOVNGCHIMI","De la Rose",
-    "TINI","María Becerra","Lali","Emilia Mernes","Nicki Nicole","Cazzu","Nathy Peluso","Soledad Pastorutti",
-    "Abel Pintos","Luciano Pereyra","Sergio Denis","Axel","Diego Torres","Fito Páez","Charly García","Gustavo Cerati",
-    "Luis Alberto Spinetta","Andrés Calamaro","Vicentico","Paulo Londra","Duki","Bizarrap","Trueno",
-    "Tiago PZK","Khea","Lit Killah","Rusherking","Wos","La Joaqui","Milo J","Luck Ra","BM","FMK",
-    "Callejero Fino","Emanero","Pablo Lescano","Rodrigo","La Mona Jiménez","Ulises Bueno",
-    "Miranda","Soda Stereo","Patricio Rey y sus Redonditos de Ricota","Los Fabulosos Cadillacs","Divididos","Las Pelotas","La Renga",
-    "Los Piojos","Babasónicos","Bersuit Vergarabat","Ratones Paranoicos","Enanitos Verdes","Attaque 77","2 Minutos","Rata Blanca",
-    "Almafuerte","Pappo's Blues","Pescado Rabioso","Sui Generis","Serú Girán","Virus","Turf","Los Tipitos","Los Brujos",
-    "Los Pericos","Los Auténticos Decadentes","Estelares","Guasones","Los Gardelitos","Viejas Locas","Intoxicados","Airbag",
-    "Ciro y los Persas","La Beriso","Las Pastillas del Abuelo","Tan Biónica","El Mató a un Policía Motorizado",
-    "Polimá Westcoast","Pablo Chill-E","Cris MJ","Canserbero",
-    "Shakira","Karol G","Maluma","J Balvin","Feid","Sebastián Yatra","Camilo","Manuel Turizo",
-    "Carlos Vives","Blessd","Ryan Castro","Beéle","Morat","Ricardo Montaner","Ovy on the Drums",
-    "Luis Miguel","Cristian Castro","Peso Pluma","Natanael Cano","Reik ","Maná (Fher Olvera)","Sin Bandera (Noel Schajris)",
-    "Romeo Santos","Prince Royce","Natti Natasha","Tokischa","El Alfa","Zion & Lennox",
-    "Natalia Oreiro","Emiliano Brancciari (NTVG)","Sebastián Teysera (La Vela Puerca)",
+    "Bad Bunny","Daddy Yankee","Don Omar","Wisin","Yandel","Rauw Alejandro","Ozuna","Anuel AA","Tego Calderón","Residente",
+    "Luis Fonsi","Ricky Martin","Chayanne","Pedro Capó","Nicky Jam","Farruko","De La Ghetto","Ñengo Flow","Arcángel","Zion",
+    "Lennox","Plan b","Lunay","Myke Towers","Mora","Lyanno","Bryant Myers","Alex Rose","Dalex","Jay Wheeler","Almighty",
+    "Cosculluela","Jowell y Randy","Kendo Kaponi","Anonimus","Young Miko","Villano Antillano","YOVNGCHIMI","De la Rose",
+    "TINI","María Becerra","Lali","Emilia Mernes","Nicki Nicole","Cazzu","Nathy Peluso","Soledad Pastorutti","Abel Pintos",
+    "Luciano Pereyra","Sergio Denis","Axel","Diego Torres","Fito Páez","Charly García","Gustavo Cerati","Luis Alberto Spinetta",
+    "Andrés Calamaro","Vicentico","Paulo Londra","Duki","Bizarrap","Trueno","Tiago PZK","Khea","Lit Killah","Rusherking",
+    "Wos","La Joaqui","Milo J","Luck Ra","BM","FMK","Callejero Fino","Emanero","Pablo Lescano","Rodrigo","La Mona Jiménez",
+    "Ulises Bueno","Miranda","Soda Stereo","Patricio Rey y sus Redonditos de Ricota","Los Fabulosos Cadillacs","Divididos",
+    "Las Pelotas","La Renga","Los Piojos","Babasónicos","Bersuit Vergarabat","Ratones Paranoicos","Enanitos Verdes",
+    "Attaque 77","2 Minutos","Rata Blanca","Almafuerte","Pappo's Blues","Pescado Rabioso","Sui Generis","Serú Girán",
+    "Virus","Turf","Los Tipitos","Los Brujos","Los Pericos","Los Auténticos Decadentes","Estelares","Guasones",
+    "Los Gardelitos","Viejas Locas","Intoxicados","Airbag","Ciro y los Persas","La Beriso","Las Pastillas del Abuelo",
+    "Tan Biónica","El Mató a un Policía Motorizado","Polimá Westcoast","Pablo Chill-E","Cris MJ","Canserbero","Shakira",
+    "Karol G","Maluma","J Balvin","Feid","Sebastián Yatra","Camilo","Manuel Turizo","Carlos Vives","Blessd","Ryan Castro",
+    "Beéle","Morat","Ricardo Montaner","Ovy on the Drums","Luis Miguel","Cristian Castro","Peso Pluma","Natanael Cano",
+    "Reik ","Maná (Fher Olvera)","Sin Bandera (Noel Schajris)","Romeo Santos","Prince Royce","Natti Natasha","Tokischa",
+    "El Alfa","Zion & Lennox","Natalia Oreiro","Emiliano Brancciari (NTVG)","Sebastián Teysera (La Vela Puerca)",
     "Roberto Musso (El Cuarteto de Nos)","Agustín Casanova (Márama)","Fernando Vázquez (Rombai)","Franny Glass",
-    "Ricardo Montaner","Chino & Nacho","Danny Ocean",
-    "Rosalía","C. Tangana","Aitana","Quevedo","Lola Índigo","Rels B","Bad Gyal","David Bisbal","Alejandro Sanz","Beret",
-    "Leiva","Enrique Iglesias","Julio Iglesias","Omar Montes",
-    "Sech","Boza","Joey Montana",
-    "Camila Cabello"
+    "Ricardo Montaner","Chino & Nacho","Danny Ocean","Rosalía","C. Tangana","Aitana","Quevedo","Lola Índigo","Rels B",
+    "Bad Gyal","David Bisbal","Alejandro Sanz","Beret","Leiva","Enrique Iglesias","Julio Iglesias","Omar Montes",
+    "Sech","Boza","Joey Montana","Camila Cabello"
   ],
 
   // ===== Jerarquía de Fútbol =====
   "Fútbol": {
     "🌍 Jugadores actuales (mundo)": [
-      'Vinícius Júnior','Jude Bellingham','Rodrygo Goes','Thibaut Courtois','Fede Valverde','Aurélien Tchouaméni','Éder Militão','Eduardo Camavinga','Toni Kroos','Luka Modrić','Franco Mastantuono','Robert Lewandowski','Lamine Yamal','Antoine Griezmann','Jan Oblak','João Félix','Raphinha','Ronald Araújo','Frenkie de Jong','Pedri','Gavi','Álvaro Morata','Koke','Iñaki Williams','Erling Haaland','Kevin De Bruyne','Rodri','Phil Foden','Julián Álvarez','Bernardo Silva','Rúben Dias',
-      'Jack Grealish','Mohamed Salah','Virgil van Dijk','Alisson Becker','Trent Alexander-Arnold','Luis Díaz','Bukayo Saka','Martin Ødegaard','Declan Rice','William Saliba','Gabriel Martinelli','Cole Palmer','Enzo Fernández','Moisés Caicedo','Bruno Fernandes','Marcus Rashford','Harry Kane','Jamal Musiala','Joshua Kimmich','Manuel Neuer','Thomas Müller','Matthijs de Ligt','Jadon Sancho','Julian Brandt','Florian Wirtz','Exequiel Palacios','Lautaro Martínez',
-      'Dibu Martínez','Nicolò Barella','Federico Dimarco','Federico Chiesa','Adrien Rabiot','Rafael Leão','Theo Hernández','Khvicha Kvaratskhelia','Victor Osimhen','Kylian Mbappé','Ousmane Dembélé','Marquinhos','Achraf Hakimi','Roberto Firmino','Marcelo','Gianluigi Donnarumma','Vitinha','Cristiano Ronaldo','Neymar Jr.','Karim Benzema','Sadio Mané',"N'Golo Kanté",'Kalidou Koulibaly','Lionel Messi','Luis Suárez','Sergio Busquets','Jordi Alba','Lorenzo Insigne','Mauro Icardi','Romelu Lukaku',
-      'Alejandro Garnacho',"Pierre-Emerick Aubameyang","Alexis Sánchez","Arturo Vidal"
+      "Vinícius Júnior","Jude Bellingham","Rodrygo Goes","Thibaut Courtois","Fede Valverde","Aurélien Tchouaméni","Éder Militão",
+      "Eduardo Camavinga","Toni Kroos","Luka Modrić","Franco Mastantuono","Robert Lewandowski","Lamine Yamal","Antoine Griezmann",
+      "Jan Oblak","João Félix","Raphinha","Ronald Araújo","Frenkie de Jong","Pedri","Gavi","Álvaro Morata","Koke","Iñaki Williams",
+      "Erling Haaland","Kevin De Bruyne","Rodri","Phil Foden","Julián Álvarez","Bernardo Silva","Rúben Dias","Jack Grealish",
+      "Mohamed Salah","Virgil van Dijk","Alisson Becker","Trent Alexander-Arnold","Luis Díaz","Bukayo Saka","Martin Ødegaard",
+      "Declan Rice","William Saliba","Gabriel Martinelli","Cole Palmer","Enzo Fernández","Moisés Caicedo","Bruno Fernandes",
+      "Marcus Rashford","Harry Kane","Jamal Musiala","Joshua Kimmich","Manuel Neuer","Thomas Müller","Matthijs de Ligt",
+      "Jadon Sancho","Julian Brandt","Florian Wirtz","Exequiel Palacios","Lautaro Martínez","Dibu Martínez","Nicolò Barella",
+      "Federico Dimarco","Federico Chiesa","Adrien Rabiot","Rafael Leão","Theo Hernández","Khvicha Kvaratskhelia","Victor Osimhen",
+      "Kylian Mbappé","Ousmane Dembélé","Marquinhos","Achraf Hakimi","Roberto Firmino","Marcelo","Gianluigi Donnarumma","Vitinha",
+      "Cristiano Ronaldo","Neymar Jr.","Karim Benzema","Sadio Mané","N'Golo Kanté","Kalidou Koulibaly","Lionel Messi","Luis Suárez",
+      "Sergio Busquets","Jordi Alba","Lorenzo Insigne","Mauro Icardi","Romelu Lukaku","Alejandro Garnacho","Pierre-Emerick Aubameyang",
+      "Alexis Sánchez","Arturo Vidal"
     ],
 
     "⭐ Leyendas (retirados)": [
-      "Diego Maradona","Pelé","Johan Cruyff","Roberto Baggio","David Beckham",
-      "Franz Beckenbauer","Alfredo Di Stéfano","Gerd Müller","Michel Platini",
-      "Zinedine Zidane","Ronaldo Nazário","Thierry Henry","Ronaldinho","Paolo Maldini",
-      "Eric Cantona","Gianluigi Buffon","Giorgio Chiellini","Andrés Iniesta",
-      "Xavi Hernández","Sergio Ramos","Andrea Pirlo","Francesco Totti",
-      "Kaká","Samuel Eto'o","Didier Drogba","Frank Lampard","Steven Gerrard",
-      "Philipp Lahm","Arjen Robben","Wesley Sneijder","Wayne Rooney",
-      "Bastian Schweinsteiger","Iker Casillas","Carles Puyol","Fabio Cannavaro",
-      "Alessandro Nesta","Roberto Carlos","Cafú","Javier Zanetti","Gerard Piqué",
-      "Marcelo","Oliver Kahn","Edwin van der Sar","Peter Schmeichel","Juan Sebastián Verón",
-      "Alessandro Del Piero","Pep Guardiola","Sergio Agüero","Gareth Bale","Eden Hazard","Paulo Dybala",
-      "Radamel Falcao","James Rodríguez","Zlatan Ibrahimović","Petr Čech","Hugo Lloris","Keylor Navas",
-      "Dani Alves","Thiago Silva","Pepe","David Villa","Fernando Torres","Xabi Alonso","Cesc Fàbregas","David Silva",
-      "Patrick Vieira","David Trezeguet","Franck Ribéry","Luís Figo","Lev Yashin","Eusébio","Bobby Charlton","Michael Owen",
-      "Rio Ferdinand","Ashley Cole","Lothar Matthäus","Miroslav Klose","Michael Ballack","Marco van Basten","Ruud Gullit",
-      "Romário","Rivaldo","Sócrates","Gabriel Batistuta","Hernán Crespo","Juan Román Riquelme","Carlos Tevez","Ariel Ortega",
-      "Fernando Redondo","Daniel Passarella","Mario Kempes","Óscar Ruggeri","Enzo Francescoli","Diego Forlán",
-      "Carlos Valderrama","René Higuita","José Luis Chilavert","Yaya Touré"
+      "Diego Maradona","Pelé","Johan Cruyff","Roberto Baggio","David Beckham","Franz Beckenbauer","Alfredo Di Stéfano",
+      "Gerd Müller","Michel Platini","Zinedine Zidane","Ronaldo Nazário","Thierry Henry","Ronaldinho","Paolo Maldini",
+      "Eric Cantona","Gianluigi Buffon","Giorgio Chiellini","Andrés Iniesta","Xavi Hernández","Sergio Ramos","Andrea Pirlo",
+      "Francesco Totti","Kaká","Samuel Eto'o","Didier Drogba","Frank Lampard","Steven Gerrard","Philipp Lahm","Arjen Robben",
+      "Wesley Sneijder","Wayne Rooney","Bastian Schweinsteiger","Iker Casillas","Carles Puyol","Fabio Cannavaro","Alessandro Nesta",
+      "Roberto Carlos","Cafú","Javier Zanetti","Gerard Piqué","Marcelo","Oliver Kahn","Edwin van der Sar","Peter Schmeichel",
+      "Juan Sebastián Verón","Alessandro Del Piero","Pep Guardiola","Sergio Agüero","Gareth Bale","Eden Hazard","Paulo Dybala",
+      "Radamel Falcao","James Rodríguez","Zlatan Ibrahimović","Petr Čech","Hugo Lloris","Keylor Navas","Dani Alves","Thiago Silva",
+      "Pepe","David Villa","Fernando Torres","Xabi Alonso","Cesc Fàbregas","David Silva","Patrick Vieira","David Trezeguet",
+      "Franck Ribéry","Luís Figo","Lev Yashin","Eusébio","Bobby Charlton","Michael Owen","Rio Ferdinand","Ashley Cole",
+      "Lothar Matthäus","Miroslav Klose","Michael Ballack","Marco van Basten","Ruud Gullit","Romário","Rivaldo","Sócrates",
+      "Gabriel Batistuta","Hernán Crespo","Juan Román Riquelme","Carlos Tevez","Ariel Ortega","Fernando Redondo","Daniel Passarella",
+      "Mario Kempes","Óscar Ruggeri","Enzo Francescoli","Diego Forlán","Carlos Valderrama","René Higuita","José Luis Chilavert",
+      "Yaya Touré"
     ],
 
     "🇦🇷 Liga Argentina (actuales)": [
@@ -80,10 +101,10 @@ export const topics = {
       "Federico Girotti"
     ],
 
-    "🏟️ Equipos" : [
-      "Boca Juniors","River Plate","Independiente","Racing Club","Estudiantes de La Plata","San Lorenzo",
-      "Vélez Sarsfield","Rosario Central","Newell's Old Boys","Gimnasia y Esgrima La Plata","Talleres de Córdoba","Argentinos Juniors",
-      "Lanús","Huracán","Defensa y Justicia","Godoy Cruz","Barracas","Platense",
+    "🏟️ Equipos": [
+      "Boca Juniors","River Plate","Independiente","Racing Club","Estudiantes de La Plata","San Lorenzo","Vélez Sarsfield","Rosario Central",
+      "Newell's Old Boys","Gimnasia y Esgrima La Plata","Talleres de Córdoba","Argentinos Juniors","Lanús","Huracán","Defensa y Justicia",
+      "Godoy Cruz","Barracas","Platense",
       // LaLiga (España)
       "Real Madrid","FC Barcelona","Atlético de Madrid","Sevilla FC","Valencia CF","Villarreal CF","Real Sociedad","Athletic Club","Real Betis","Girona FC",
       // Premier League (Inglaterra)
@@ -102,19 +123,48 @@ export const topics = {
       "Inter Miami CF",
       // Brasileirão (Brasil)
       "Flamengo","Palmeiras","Corinthians","São Paulo FC","Santos FC","Grêmio","Internacional","Fluminense","Atlético Mineiro","Botafogo","Athletico Paranaense","Fortaleza",
-      // Primera División (Uruguay)
+      // Uruguay
       "Nacional","Peñarol",
-      // Primera División (Chile)
+      // Chile
       "Colo-Colo","Universidad de Chile","Universidad Católica",
-      // Serie A (Ecuador)
+      // Ecuador
       "LDU Quito","Barcelona SC","Independiente del Valle",
-      // Categoría Primera A (Colombia)
+      // Colombia
       "Atlético Nacional","Millonarios FC","América de Cali","Junior FC",
-      // Primera División (Paraguay)
+      // Paraguay
       "Olimpia","Cerro Porteño","Libertad",
-      // Liga 1 (Perú)
+      // Perú
       "Alianza Lima","Universitario"
     ],
+
+    // Se rellena luego (abajo) con la suma de listas
     "👥 Jugadores (todos)": []
   }
 };
+
+// ===== Post-procesado: normaliza y elimina alias que duplican en el UI =====
+(() => {
+  const F = topics["Fútbol"];
+  if (!F) return;
+
+  // Asegurar "⭐ Leyendas" y remover la variante entre paréntesis del UI
+  if (F["⭐ Leyendas (retirados)"] && !F["⭐ Leyendas"]) {
+    F["⭐ Leyendas"] = F["⭐ Leyendas (retirados)"];
+  }
+  delete F["⭐ Leyendas (retirados)"];
+
+  // Construir 👥 Jugadores (todos) = actuales + liga arg + leyendas (únicos)
+  const ALL = [
+    ...(F["🌍 Jugadores actuales (mundo)"] || []),
+    ...(F["🇦🇷 Liga Argentina (actuales)"] || []),
+    ...(F["⭐ Leyendas"] || [])
+  ];
+  F["👥 Jugadores (todos)"] = Array.from(new Set(ALL));
+
+  // Remover alias que sólo generaban duplicados en el select
+  delete F["Jugadores (todos)"];
+  delete F["Todos los jugadores"];
+  delete F["🏟️ Equipos (Argentina)"];
+  delete F["🏟️ Equipos (Internacionales)"];
+})();
+
